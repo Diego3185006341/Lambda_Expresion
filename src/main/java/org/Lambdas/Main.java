8d79435cd81ea1aebac2425349eb4e219e153b85
Diego3185006341 <90737409+Diego3185006341@users.noreply.github.com>
2023-11-27 21:36:57 -0500

    commit2 : finish

--- src/main/java/org/Lambdas/Main.java
+++ src/main/java/org/Lambdas/Main.java
@@ -3,10 +3,9 @@ package org.Lambdas;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,6 +14,8 @@ public class Main {
         Person p1 = new Person("elis",LocalDate.of(1991, 1, 21),34546);
         Person p2 = new Person("carlos ",LocalDate.of(1995, 2, 11),64546);
         Person p3 = new Person("albert ",LocalDate.of(2000, 1, 06),78453);
+        Person p4 = new Person("chavo ",LocalDate.of(2005, 11, 06),78453);
+        Person p5 = new Person("penelope ",LocalDate.of(2010, 10, 06),78453);
 
         Employee e1 = new Employee("alfed",LocalDate.of(1990, 1, 7),34546);
         Employee e2 = new Employee("dino ",LocalDate.of(1980, 2, 7),64546);
@@ -26,7 +27,7 @@ public class Main {
         Products pr4 = new Products(4, "Ceviche", 15.0);
 
 
-        List<Person> persons = Arrays.asList(p1,p2,p3);
+        List<Person> persons = Arrays.asList(p1,p2,p3,p4,p5);
         List<Employee> employees = Arrays.asList(e1,e2,e3);
         List<Products> products = Arrays.asList(pr1, pr2, pr3, pr4);
 
@@ -66,7 +67,7 @@ public class Main {
         ListMapper(collect);*/
 
         //sorted
-
+/*
         Comparator<Person> byNameAsc = Comparator.comparing(Person::getName);
         List<Person> personList = persons.stream()
                 .sorted(byNameAsc).collect(Collectors.toList());
@@ -76,7 +77,7 @@ public class Main {
         List<Person> personLista = persons.stream()
                 .sorted(byNameDesc)
                 .collect(Collectors.toList());
-        ListMapper(personLista);
+        ListMapper(personLista);*/
 
         //filter
         /*
@@ -93,9 +94,82 @@ public class Main {
 
         List<Integer> compare = collectemAge.stream()
                 .filter(ageemployees  ->  persons.
-                        stream().noneMatch(person -> ageemployees.equals(person.getEdad()))).collect(Collectors.toList());
+                        stream().noneMatch(person -> ageemployees.equals(person.getEdad())))
+                        .collect(Collectors.toList());
 
-        ListMapper(compare);*/
+
+        ListMapper(compare);
+
+        Predicate<Person> startWith = name -> name.getName().startsWith("a");
+        boolean rsPerson = persons.stream()
+                .anyMatch(startWith);
+        boolean rsPerson1 = persons.stream()
+                .noneMatch(p -> p.getName().startsWith("z"));
+        boolean rsPerson2 = persons.stream()
+                .allMatch(p -> p.getName().startsWith("a"));
+
+
+        System.out.println(rsPerson);
+        System.out.println(rsPerson1);
+        System.out.println(rsPerson2);
+*/
+
+        //skip limit pagination
+      /*  int pageSize =2;
+        int pageNumber = 1;
+
+        persons.
+                stream()
+                .skip(pageSize * pageNumber)
+                .limit(pageSize)
+                .collect(Collectors.toList())
+                .forEach(System.out::print);*/
+
+        //Collectors Groupby
+
+/*
+        Map<Double, List<Products>> collect1 = products.stream()
+                .filter(p -> p.getPrice() > 20)
+                .collect(Collectors.groupingBy(Products::getPrice));
+        System.out.println(collect1);
+*/
+
+        //counting
+/*
+        Map<String, Long> collect2 = products.stream()
+                .collect(Collectors.groupingBy(
+                        Products::getName,Collectors.counting()
+                )
+                );
+        System.out.println(collect2);*/
+
+
+        //grupingby name and sum em after that
+
+        /*Map<String, Double> collect3 = products.stream()
+                .collect(Collectors.groupingBy(
+                                Products::getName,Collectors.summingDouble(Products ::getPrice)
+                        )
+                );
+        System.out.println(collect3);*/
+
+        //summary
+
+      /*  DoubleSummaryStatistics statistics = products.stream()
+                .collect(Collectors.summarizingDouble(Products::getPrice));
+
+        long df = products.size();
+
+        System.out.println(statistics.getAverage());
+        System.out.println(df);
+        System.out.println(statistics);*/
+
+        Optional<Double> reduce = products.stream()
+                .map(Products::getPrice)
+                .reduce(Double::sum);
+
+        System.out.println(reduce);
+
 
         //filter employee by name
 
